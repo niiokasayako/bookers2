3b8acfb20a7e0a2cbc40805b48088f53ae1ceffe
niiokasayako <107908957+niiokasayako@users.noreply.github.com>
2022-07-12 03:28:40 +0000

    [Start]アプリケーション作成：応用編-課題

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,38 @@
+class BooksController < ApplicationController
+  def new
+    @book = Book.new
+  end
+  
+  def create
+    @book = Book.new(book_params)
+    @book.user_id = current_user.id
+    if @book.save
+       redirect_to book_path
+    else
+       render :new
+    end
+  end
+
+  def index
+    @books = Book.page(params[:page]).reverse_order
+  end
+
+  def show
+    @book = Book.find(params[:id])
+  end
+  
+  def destroy
+    @book = Book.find(params[:id])
+    @book.destroy
+    redirect_to book_path
+  end
+  
+  #投稿データのストロングパラメータ
+  private
+  
+  def book_params
+    params.require(:book).permit(:title, :body)
+  end
+  
+end
+end
---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,31 @@
+class UsersController < ApplicationController
+  
+  def new
+    @user = User.new
+  end
+  
+  def index
+  end
+ 
+  def show
+    @user = User.find(params[:id])
+    @books = @user.books.page(params[:page]).reverse_order
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+  
+  def update
+    @user = User.find(params[:id])
+    @user.update(user_params)
+    redirect_to user_path(@user.id)
+  end
+  
+  private
+  
+  def user_params
+    params.require(:user).permit(:name, :introduction)
+  end
+  
+end
